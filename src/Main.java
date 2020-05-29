e125b4192e08c205aa525a8ee70a1805c178826c
Chris <chrisk844@gmail.com>
2020-05-28 23:05:24 -0600

    Artifacts

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -15,12 +17,34 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         BorderPane bp = new BorderPane();
         VBox vb = new VBox();
-        bp.setCenter(vb);
         Button random = new Button("Connect to random server");
         random.setOnAction(e -> {
-            System.out.println("");
+            try {
+                Process ra = Runtime.getRuntime().exec("protonvpn c -r");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         });
         vb.getChildren().add(random);
+        Button fastest = new Button("Connect to fastest server");
+        fastest.setOnAction(e -> {
+            try {
+                Process fa = Runtime.getRuntime().exec("protonvpn c -f");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        vb.getChildren().add(fastest);
+        Button disconnect = new Button("Disconnect");
+        disconnect.setOnAction(e -> {
+            try {
+                Process di = Runtime.getRuntime().exec("protonvpn c -f");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        vb.getChildren().add(disconnect);
+        bp.setCenter(vb);
         Scene scn = new Scene(bp);
         primaryStage.setScene(scn);
         primaryStage.show();
